53a8ce8b812973824fd7566646a9b6def1fa3a17
a.zemlyanenko <a.zemlyanenko@lanet.ua>
2018-05-16 11:23:50 +0300

    Create component getting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	points = [253, 9, 11, 431, 278, 179, 81, 249, 416, 169, 439, 481, 481, 186, 314, 484, 40, 421, 419, 241];
 }
